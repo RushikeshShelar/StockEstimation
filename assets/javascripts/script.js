eff1592c5e0875203f9c9b292ce23ce9aa6f3c92
Rushi <rushikeshshelar.cs@gmail.com>
2023-10-11 23:15:13 +0530

    Added Home Page and Registration Form

--- assets/javascripts/script.js
+++ assets/javascripts/script.js
@@ -7,17 +7,49 @@ menuIcon.onclick = () => {
     navbar.classList.toggle("active");
 };
 const loginForm = document.querySelector("#loginForm");
+const registerForm = document.querySelector("#registerForm");
 const login = document.querySelector("#login");
+const register = document.querySelector("#register");
 
+const heroSection = document.querySelector(".hero");
+const startBtn = document.querySelector("#getStart");
+
+const registerIn = document.querySelector("#register-in");
+const loginIn = document.querySelector("#log-in");
+
+registerIn.onclick = () => {
+    registerForm.classList.add("active");
+    heroSection.classList.add("active");
+    loginForm.classList.remove("active");
+};
+
+loginIn.onclick = () => {
+    loginForm.classList.add("active");
+    heroSection.classList.add("active");
+    registerForm.classList.remove("active");
+};
 
 login.onclick = () => {
     loginForm.classList.add("active");
+    heroSection.classList.add("active");
 };
 
-const submitButton = document.querySelector("#submit");
+register.onclick = () => {
+    registerForm.classList.add("active");
+    heroSection.classList.add("active");
+};
+
+startBtn.onclick = () => {
+    loginForm.classList.add("active");
+    startBtn.classList.add("active");
+    heroSection.classList.add("active");
+}
+
+const submitButton = document.querySelector(".submit");
 submitButton.addEventListener("click", (e) => {
     e.preventDefault();
     submitButton.onclick = () => {
         loginForm.classList.remove("active");
+        heroSection.classList.remove("active");
     };
 });
